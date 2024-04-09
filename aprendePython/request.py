16cf2d895a1f7e326be16a160242eac58f5630d4
e\Ela Lino <87192835+LinoELa@users.noreply.github.com>
2024-04-09 14:09:23 +0200

    R: request --> Obtener informacion de la cabecera como seria : URL , tipo de metadado , proveedor de server

--- aprendePython/request.py
+++ aprendePython/request.py
@@ -1,36 +1,14 @@
 import requests
 
+#optener la cabecera 
 
-# CONTENIDO JSON
+pagina = requests.get('https://casaruralzaragoza.com/')
+# print(pagina.text)
 
-sc_tnerife = (28.4578025, -16.3563748)
+print(pagina.url)
+print(pagina.status_code)
 
-parametros_1 = {
-    'latitude' : sc_tnerife[0], 
-    'longitude' : sc_tnerife[1], 
-    'hourly' : 'temperature_2m'
-    }
-
-url_del_api = 'https://api.open-meteo.com/v1/forecast'
-
-respuesta = requests.get(url_del_api, params=parametros_1)
-
-print(respuesta.url)
-
-datos = respuesta.json()
-# print(datos)
-# dict
-print(datos.keys())
-
-
-
-# QUEREMOS MOSTRAR DE DE UN SOLO DIA 
-
-temperaturas = datos['hourly']['temperature_2m']
-
-for i, temp in enumerate(temperaturas[24:48], start=1):
-    print(f'{temp:4.1f}', end='---')
-    if i % 6 == 0 :
-        print()
+print(pagina.headers.get('Content-Type'))
+print(pagina.headers.get('Server'))
 
 
