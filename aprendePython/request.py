b9fb6444563951f767e34f4ee867b6834d68bc83
e\Ela Lino <87192835+LinoELa@users.noreply.github.com>
2024-04-09 14:23:25 +0200

    R:request --> Descargar , Crear ficheros planos y binarios

--- aprendePython/request.py
+++ aprendePython/request.py
@@ -1,14 +1,36 @@
 import requests
 
-#optener la cabecera 
+#   DESCARGAR FICHEROS
 
-pagina = requests.get('https://casaruralzaragoza.com/')
-# print(pagina.text)
 
-print(pagina.url)
-print(pagina.status_code)
+# fichero en texto plano
+        #se crea un ficchero data.csv y se le escribe dentro
+url_fichero_plano = 'https://www.ine.es/jaxi/files/tpx/es/csv_bdsc/50155.csv'
 
-print(pagina.headers.get('Content-Type'))
-print(pagina.headers.get('Server'))
+pagina = requests.get(url_fichero_plano)
+
+pagina.status_code
+
+
+with open('data.csv' , 'w') as f : 
+    f.write(pagina.text)
+
+
+#Fichero Binario
+
+url_fichero_binario = 'https://www.ine.es/jaxi/files/tpx/es/csv_bdsc/50155.csv'
+
+pagina_2 = requests.get(url_fichero_binario)
+
+pagina_2.status_code
+
+with open('datos_fichero_binario', 'w') as f:
+    f.write(pagina_2.content) #
+            # TypeError: write() argument must be str, not bytes
+
+
+            
+
+
 
 
